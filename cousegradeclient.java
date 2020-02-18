01fa7ba9ef20b260d9852cccc6e2b618698ab579
Shijo T <yogscast2ndgen@gmail.com>
2020-02-18 13:08:41 -0500

    Update cousegradeclient.java

--- cousegradeclient.java
+++ cousegradeclient.java
@@ -1,14 +1,14 @@
-class Main
+// CS2511 Chapter 7 Group Lab
+// Group 3: Antonio Codignotto, Shijo Thomas, Kevin Jaimon
+
+
+class coursegradeclient
 {
   public static void main(String[] args)
   {
-    CourseGrade APCS = new CourseGrade("Computer Science", 'A');
-    CourseGrade C2 = new CourseGrade("Coding II", 'B');
-    APCS.setName("AP Computer Science");
-    C2.setGrade('A');
-    System.out.println(APCS.getName());
-    System.out.println(C2.getGrade());
-    System.out.println(C2.toString());
-    System.out.println(APCS.equals(C2));
+    Grade g1;
+
+    g1 = new Grade();
+    
   }
 }
