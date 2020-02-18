77d4fe33428faba2d60ad81157df621342e83174
kjaimon <61204004+kjaimon@users.noreply.github.com>
2020-02-18 12:59:27 -0500

    Update cousegradeclient.java

--- cousegradeclient.java
+++ cousegradeclient.java
@@ -1,35 +1,14 @@
-// Needs to be refomatted for coursegrade
-
-public class CourseGrade
+class Main
 {
-	public static void main(String [] args)
-	{
-		Grade g1;
-
-		// test constructors
-		c1 = new Circle();
-
-		Circle c2 = new Circle(4.0);
-
-		// test accessor
-		double y = c2.getRadius();
-
-		System.out.println("The radius of c2 is " + y);
-
-		// test mutator
-		c1.setRadius(3.0);
-
-		System.out.println("The radius of c1 is " + c1.getRadius());
-
-		// test toString()
-		// explicit call
-		System.out.println(c1.toString());
-		// implicit call
-		System.out.println(c2);
-
-		if (c1.equals(c2))
-				System.out.println("Hi");
-		else
-				System.out.println("Bye");
-	}
+  public static void main(String[] args)
+  {
+    CourseGrade APCS = new CourseGrade("Computer Science", 'A');
+    CourseGrade C2 = new CourseGrade("Coding II", 'B');
+    APCS.setName("AP Computer Science");
+    C2.setGrade('A');
+    System.out.println(APCS.getName());
+    System.out.println(C2.getGrade());
+    System.out.println(C2.toString());
+    System.out.println(APCS.equals(C2));
+  }
 }
